2d1b3e7bb171405f440964c4cc211bc235093b95
ZakorkoVarya <zakorko.varya@mail.ru>
2026-05-08 17:40:01 +0300

    Add files via upload

---
+++ Lab 4/Ttask.java
@@ -0,0 +1,40 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalTime;
+
+public class Ttask {
+    
+    public static void log(Exception e) {
+        try (FileWriter writer = new FileWriter("log.txt", true)) {
+            String timestamp = LocalTime.now().toString();
+            writer.write(timestamp + " - " + e.getMessage() + "\n");
+        } catch (IOException ioEx) {
+            System.out.println("Error while logging: " + ioEx.getMessage());
+        }
+    }
+    
+    public static int parse(String s) throws CustomNumberFormatException {
+        try {
+            return Integer.parseInt(s);
+        } catch (NumberFormatException e) {
+            throw new CustomNumberFormatException("String is not a number: " + s, e);
+        }
+    }
+    
+    public static void main(String[] args) {
+        String[] data = {"10", "20", "abc", "30"};
+        
+        for (String s : data) {
+            try {
+                int number = parse(s);
+                System.out.println("Number: " + number);
+            } catch (CustomNumberFormatException e) {
+                System.out.println("Error: " + e.getMessage());
+                log(e);
+            }
+        }
+    }
+}
+
+
+
